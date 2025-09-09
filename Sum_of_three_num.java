45a8013c372255cb65d829df9ad85a832990974a
kadheeskaad <kadheeskaabalan@gmail.com>
2025-09-09 20:30:35 +0530

    Add files via upload

---
+++ Sum_of_three_num.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Sum_of_three_num
+{
+	public static void main(String args[])
+	{
+		int a=5;
+		int b=10;
+		int c=15;
+		System.out.println(a+b+c);
+	}
+}
---
+++ Sum_of_two_num.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Sum_of_two_num
+{
+	public static void main(String args[])
+	{
+		int a=5;
+		int b=15;
+		System.out.println(a+b);
+	}
+}
