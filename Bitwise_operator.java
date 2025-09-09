dfd27373f086df144e162ae22d3f003c4c519d97
kadheeskaad <kadheeskaabalan@gmail.com>
2025-09-09 20:12:42 +0530

    Add files via upload

---
+++ Bitwise_operator.java
@@ -0,0 +1,14 @@
+class Bitwise_operator
+{
+	public static void main(String[] args) 
+	{
+	    int a = 492;
+	    int b = 333;
+	    int c = a&b;
+	    int d = a|b;
+	    int e = a^b;
+	    System.out.println (c);
+	    System.out.println (d);
+	    System.out.println (e);
+	}
+}
