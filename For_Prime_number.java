cf5b0032058ea0f1adb1ca2fea9a3871a2704c03
kadheeskaad <kadheeskaabalan@gmail.com>
2025-09-15 20:26:36 +0530

    Add files via upload

---
+++ For_Prime_number.java
@@ -0,0 +1,17 @@
+class For_Prime_number
+{
+	public static void main(String[] args)
+	{
+	    for ( int i=2;i<=10;i++)
+	    {
+	        int count = 0;
+	        for (int j=1;j<=i;j++)
+	        {
+	            if (i%j == 0)
+	            count ++;
+	        }
+	        if(count == 2)
+	        System.out.println(i);
+	    }
+	}
+}
