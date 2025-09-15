83846d8c1e1bd1180817ae0469671319eb953395
kadheeskaad <kadheeskaabalan@gmail.com>
2025-09-15 20:34:05 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,13 @@
+class Increment
+{
+    public static void main(String[] args) 
+    {
+        int x=10;
+        int v = --x + x++ + ++x + --x + x++;
+        int z = x++ + ++x + ++x + x++ + ++x;
+        int a = --x + --x + --x + --x;
+        System.out.println(v);
+        System.out.println(z);
+        System.out.println(a);
+    }
+}
