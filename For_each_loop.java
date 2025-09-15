1e6f8bf4d8e8f669b4272aed674fae6e50aadf6b
kadheeskaad <kadheeskaabalan@gmail.com>
2025-09-15 20:32:05 +0530

    Add files via upload

---
+++ For_each_loop.java
@@ -0,0 +1,10 @@
+class For_each_loop
+{
+	public static void main(String[] args) {
+	    int arr[] = { 1,2,3,4,5};
+	    for(int a : arr)
+	    {
+	        System.out.println(a + " ");
+	    }
+	}
+}
