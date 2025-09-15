bbf52deed9941c6587c9c69512f3d6f99a0f158d
kadheeskaad <kadheeskaabalan@gmail.com>
2025-09-15 20:30:00 +0530

    Add files via upload

---
+++ For_loop.java
@@ -0,0 +1,19 @@
+class For_loop
+{
+	public static void main(String[] args) 
+	{
+	    int a=7;
+	    for(;;)
+	    {
+	        if(a>3)
+	        {
+	        System.out.println(a);
+	        }
+	        else
+	        {
+	        break;
+	        }
+	        a--;
+	   }
+	}
+}
